c24ea7497edb8937206f42444a3fecdfbe0a985d
Clau <claudiomrussi@hotmail.com>
2016-07-06 03:34:50 -0300

    Add user model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, presence:true, uniqueness: true, length:{minimum:9,maximum:200}
+  validates :name, presence:true, length:{maximum:15}
+  has_many :posts
+  has_many :comments
 end
